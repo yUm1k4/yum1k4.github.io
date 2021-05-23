d8edb0b2a9f1653d200a66546c718e5ccbbe173b
yUm1k4 <zaiabdullah99@gmail.com>
2021-05-24 04:21:10 +0700

    feat: testimonial

--- js/main.js
+++ js/main.js
@@ -171,4 +171,49 @@ function bodyScrollingToggle() {
             popup.scrollTo(0, projectDetailsContainer.offsetTop)
         }
     }
-})()
+})();
+
+// Testimonial slider
+(() => {
+    const sliderContainer = document.querySelector('.testi-slider-container'),
+    slides = sliderContainer.querySelectorAll('.testi-item'),
+    slideWidth = sliderContainer.offsetWidth,
+    prevBtn = document.querySelector('.testi-slider-nav .prev'),
+    nextBtn = document.querySelector('.testi-slider-nav .next'),
+    activeSlide = sliderContainer.querySelector('.testi-item.active');
+    let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+
+    // set width of all slides
+    slides.forEach((slide) => {
+        slide.style.width = slideWidth + 'px'
+    })
+    // set width of sliderContainer
+    sliderContainer.style.width = slideWidth * slides.length + 'px'
+
+    nextBtn.addEventListener('click', () => {
+        if (slideIndex === slides.length - 1) {
+            slideIndex = 0
+        } else {
+            slideIndex++;
+        }
+        slider()
+    })
+
+    prevBtn.addEventListener('click', () => {
+        if (slideIndex === 0) {
+            slideIndex = slides.length - 1
+        } else {
+            slideIndex--;
+        }
+        slider()
+    })
+
+    function slider() {
+        // deactivate existing activw slides
+        sliderContainer.querySelector('.testi-item.active').classList.remove('active')
+        // activate new slide
+        slides[slideIndex].classList.add('active')
+        sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + 'px'
+    }
+    slider()
+})();
