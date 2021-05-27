b6ff44597488da1c259a054ab901027eacaf6014
yUm1k4 <zaiabdullah99@gmail.com>
2021-05-28 05:26:24 +0700

    fix: update layout

--- js/main.js
+++ js/main.js
@@ -329,11 +329,11 @@ function sendMail() {
 
 // Hide all sections except active
 // for testing only capture all page
-// (() => {
-//     const sections = document.querySelectorAll('.section')
-//     sections.forEach((section) => {
-//         if (!section.classList.contains('active')) {
-//             section.classList.add('hide')
-//         }
-//     })
-// })();
+(() => {
+    const sections = document.querySelectorAll('.section')
+    sections.forEach((section) => {
+        if (!section.classList.contains('active')) {
+            section.classList.add('hide')
+        }
+    })
+})();
